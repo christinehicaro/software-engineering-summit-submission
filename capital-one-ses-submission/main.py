93148da20d27801ee88c6a40fc4ad8d9fc14340b
Christine Hicaro <christinehicaro@gmail.com>
2017-04-04 01:56:08 -0500

    made changes to incorporate api

--- capital-one-ses-submission/main.py
+++ capital-one-ses-submission/main.py
@@ -22,11 +22,11 @@ import jinja2
 import json
 import logging
 import random
-import urllib
+# import requests
+import urllib2
 from operator import eq
 import os
 import webapp2
-# import yelp
 
 
 jinja_environment = jinja2.Environment(
@@ -34,8 +34,57 @@ jinja_environment = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+# def get_lat_lng():
+#     send_url = 'http://freegeoip.net/json'
+#     r = requests.get(send_url)
+#     j = json.loads(r.text)
+#     lat = j['latitude']
+#     lon = j['longitude']
+#     console.log(lat)
+#     console.log(lon)
+
+def replace_spaces(query):
+    return query.lower().replace(',','').replace(' ', '%20')
+
+def get_current_latitude():
+    url = 'http://freegeoip.net/json'
+    response = urllib2.urlopen(urllib2.Request(url, headers={'Accept': 'application.json'}))
+    location_data = json.load(response)
+    latitude = location_data['latitude']
+    return latitude
+
+def get_current_longitude():
+    url = 'http://freegeoip.net/json'
+    response = urllib2.urlopen(urllib2.Request(url, headers={'Accept': 'application.json'}))
+    location_data = json.load(response)
+    longitude = location_data['longitude']
+    return longitude
+
+def get_latitude(location):
+    google_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&sensor=false'
+    response = urllib2.urlopen(urllib2.Request(google_url, headers={'Accept': 'application.json'}))
+    location_data = json.load(response)
+    latitude = location_data['results'][0]['geometry']['location']['lat']
+    return latitude
+
+def get_longitude(location):
+    google_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&sensor=false'
+    response = urllib2.urlopen(urllib2.Request(google_url, headers={'Accept': 'application.json'}))
+    location_data = json.load(response)
+    longitude = location_data['results'][0]['geometry']['location']['lng']
+    return longitude
+
+def get_api_contents(search_term, latitude, longitude):
+    auth = 'Bearer 0F3JznLA0qsIEAU0FRfrhuX8sJLnyHGT2Fsye18cZ50eujlFbiPSCHYbAIkVM6dv6EpZVyev_kLIqg1NwVsHf5UoLTbX9d_klxBooyOFlWNv6svWWdXB39_MFaXdWHYx'
+    yelp_url = 'https://api.yelp.com/v3/businesses/search?term=' + search_term + '&limit=3&latitude=' + str(latitude) + '&longitude=' + str(longitude)
+    response = urllib2.urlopen(urllib2.Request(yelp_url, headers={'Authorization': auth, 'Accept': 'application.json'}))
+    businesses_data = json.load(response)
+    place = businesses_data['businesses'][0]['name']
+    return place
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # get_lat_lng()
         template = jinja_environment.get_template('templates/index.html')
         self.response.write(template.render())
 
@@ -46,21 +95,37 @@ class AboutHandler(webapp2.RequestHandler):
 
 class CoffeeHandler(webapp2.RequestHandler):
     def get(self):
+        current_lat = get_current_latitude()
+        current_lng = get_current_longitude()
+        place = get_api_contents('coffee', current_lat, current_lng)
+        logging.info(place)
         template = jinja_environment.get_template('templates/coffee.html')
         self.response.write(template.render())
 
 class DonutsHandler(webapp2.RequestHandler):
     def get(self):
+        current_lat = get_current_latitude()
+        current_lng = get_current_longitude()
+        place = get_api_contents('donuts', current_lat, current_lng)
+        logging.info(place)
         template = jinja_environment.get_template('templates/donuts.html')
         self.response.write(template.render())
 
 class DrinksHandler(webapp2.RequestHandler):
     def get(self):
+        current_lat = get_current_latitude()
+        current_lng = get_current_longitude()
+        place = get_api_contents('drinks', current_lat, current_lng)
+        logging.info(place)
         template = jinja_environment.get_template('templates/drinks.html')
         self.response.write(template.render())
 
 class ResultHandler(webapp2.RequestHandler):
     def get(self):
+        location = self.request.get('searchInput')
+        location = replace_spaces(location)
+        place = get_api_contents('donuts', get_latitude(location), get_longitude(location))
+        logging.info(place)
         template = jinja_environment.get_template('templates/result.html')
         self.response.write(template.render())
 
